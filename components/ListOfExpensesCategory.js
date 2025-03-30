1366dbcfc93eb25f53199197aef2f537f0a8d283
jakemerlin1133 <jakemerlin123@gmail.com>
2025-03-30 14:34:22 +0800

    done

--- components/ListOfExpensesCategory.js
+++ components/ListOfExpensesCategory.js
@@ -21,155 +21,157 @@ const ListOfExpensesCategory = ({
         <View style={styles.container}>
 
           <TouchableOpacity>
-          <View style={styles.expensesCategory}>
-            <View>
-              <Ionicons
-                name="fast-food-outline"
-                size={24}
-                style={styles.iconStyle}
-              />
-            </View>
+            <View style={styles.expensesCategory}>
+              <View>
+                <Ionicons
+                  name="fast-food-outline"
+                  size={24}
+                  style={styles.iconStyle}
+                />
+              </View>
 
-            <View style={styles.foodStyleContainer}>
-              <Text style={styles.textCategoryandExepenses}>Foods</Text>
+              <View style={styles.foodStyleContainer}>
+                <Text style={styles.textCategoryandExepenses}>Foods</Text>
+              </View>
+              <View style={styles.expensesContainer}>
+                <Text style={styles.expensesText}>P {fooodExpenses ? Number(fooodExpenses).toLocaleString() : "0"}</Text>
+              </View>
             </View>
-            <View style={styles.expensesContainer}>
-              <Text style={styles.expensesText}>P {fooodExpenses}</Text>
-            </View>
-          </View>
           </TouchableOpacity>
 
           <TouchableOpacity>
-          <View style={styles.expensesCategory}>
-            <View>
-              <Ionicons
-                name="cart-outline"
-                size={24}
-                style={styles.iconStyle}
-              />
-            </View>
+            <View style={styles.expensesCategory}>
+              <View>
+                <Ionicons
+                  name="cart-outline"
+                  size={24}
+                  style={styles.iconStyle}
+                />
+              </View>
 
-            <View style={styles.foodStyleContainer}>
-              <Text style={styles.textCategoryandExepenses}>Grocery</Text>
+              <View style={styles.foodStyleContainer}>
+                <Text style={styles.textCategoryandExepenses}>Grocery</Text>
+              </View>
+              <View style={styles.expensesContainer}>
+                <Text style={styles.expensesText}>P {groceryExpenses ? Number(groceryExpenses).toLocaleString() : "0"}</Text>
+              </View>
             </View>
-            <View style={styles.expensesContainer}>
-              <Text style={styles.expensesText}>P {groceryExpenses}</Text>
-            </View>
-          </View>
           </TouchableOpacity>
 
           <TouchableOpacity>
-          <View style={styles.expensesCategory}>
-            <View>
-              <Ionicons
-                name="shirt-outline"
-                size={24}
-                style={styles.iconStyle}
-              />
-            </View>
+            <View style={styles.expensesCategory}>
+              <View>
+                <Ionicons
+                  name="shirt-outline"
+                  size={24}
+                  style={styles.iconStyle}
+                />
+              </View>
 
-            <View style={styles.foodStyleContainer}>
-              <Text style={styles.textCategoryandExepenses}>Shopping</Text>
+              <View style={styles.foodStyleContainer}>
+                <Text style={styles.textCategoryandExepenses}>Shopping</Text>
+              </View>
+              <View style={styles.expensesContainer}>
+                <Text style={styles.expensesText}>P {shoppingExpenses  ? Number(shoppingExpenses).toLocaleString() : "0"}</Text>
+              </View>
             </View>
-            <View style={styles.expensesContainer}>
-              <Text style={styles.expensesText}>P {shoppingExpenses}</Text>
-            </View>
-          </View>
           </TouchableOpacity>
 
           <TouchableOpacity>
-          <View style={styles.expensesCategory}>
-            <View>
-              <Ionicons
-                name="cash-outline"
-                size={24}
-                style={styles.iconStyle}
-              />
-            </View>
+            <View style={styles.expensesCategory}>
+              <View>
+                <Ionicons
+                  name="cash-outline"
+                  size={24}
+                  style={styles.iconStyle}
+                />
+              </View>
 
-            <View style={styles.foodStyleContainer}>
-              <Text style={styles.textCategoryandExepenses}>Bill</Text>
+              <View style={styles.foodStyleContainer}>
+                <Text style={styles.textCategoryandExepenses}>Bill</Text>
+              </View>
+              <View style={styles.expensesContainer}>
+                <Text style={styles.expensesText}>P {billExpenses ? Number(billExpenses).toLocaleString() : "0"}</Text>
+              </View>
             </View>
-            <View style={styles.expensesContainer}>
-              <Text style={styles.expensesText}>P {billExpenses}</Text>
-            </View>
-          </View>
           </TouchableOpacity>
 
           <TouchableOpacity>
-          <View style={styles.expensesCategory}>
-            <View>
-              <Ionicons
-                name="heart-circle-outline"
-                size={24}
-                style={styles.iconStyle}
-              />
-            </View>
+            <View style={styles.expensesCategory}>
+              <View>
+                <Ionicons
+                  name="heart-circle-outline"
+                  size={24}
+                  style={styles.iconStyle}
+                />
+              </View>
 
-            <View style={styles.foodStyleContainer}>
-              <Text style={styles.textCategoryandExepenses}>Medicine</Text>
+              <View style={styles.foodStyleContainer}>
+                <Text style={styles.textCategoryandExepenses}>Medicine</Text>
+              </View>
+              <View style={styles.expensesContainer}>
+                <Text style={styles.expensesText}>P {medicineExpenses ? Number(medicineExpenses).toLocaleString() : "0"}</Text>
+              </View>
             </View>
-            <View style={styles.expensesContainer}>
-              <Text style={styles.expensesText}>P {medicineExpenses}</Text>
-            </View>
-          </View>
           </TouchableOpacity>
 
           <TouchableOpacity>
-          <View style={styles.expensesCategory}>
-            <View>
-              <Ionicons
-                name="build-outline"
-                size={24}
-                style={styles.iconStyle}
-              />
-            </View>
+            <View style={styles.expensesCategory}>
+              <View>
+                <Ionicons
+                  name="build-outline"
+                  size={24}
+                  style={styles.iconStyle}
+                />
+              </View>
 
-            <View style={styles.foodStyleContainer}>
-              <Text style={styles.textCategoryandExepenses}>Hardware</Text>
+              <View style={styles.foodStyleContainer}>
+                <Text style={styles.textCategoryandExepenses}>Hardware</Text>
+              </View>
+              <View style={styles.expensesContainer}>
+                <Text style={styles.expensesText}>
+                  P {hardwareExpenses ? Number(hardwareExpenses).toLocaleString() : "0"}
+                </Text>
+              </View>
             </View>
-            <View style={styles.expensesContainer}>
-              <Text style={styles.expensesText}>P {hardwareExpenses}</Text>
-            </View>
-          </View>
           </TouchableOpacity>
 
           <TouchableOpacity>
-          <View style={styles.expensesCategory}>
-            <View>
-              <Ionicons
-                name="hardware-chip-outline"
-                size={24}
-                style={styles.iconStyle}
-              />
-            </View>
+            <View style={styles.expensesCategory}>
+              <View>
+                <Ionicons
+                  name="hardware-chip-outline"
+                  size={24}
+                  style={styles.iconStyle}
+                />
+              </View>
 
-            <View style={styles.foodStyleContainer}>
-              <Text style={styles.textCategoryandExepenses}>Technology</Text>
+              <View style={styles.foodStyleContainer}>
+                <Text style={styles.textCategoryandExepenses}>Technology</Text>
+              </View>
+              <View style={styles.expensesContainer}>
+                <Text style={styles.expensesText}>P {technologyExpenses ? Number(technologyExpenses).toLocaleString() : "0"}</Text>
+              </View>
             </View>
-            <View style={styles.expensesContainer}>
-              <Text style={styles.expensesText}>P {technologyExpenses}</Text>
-            </View>
-          </View>
           </TouchableOpacity>
 
           <TouchableOpacity>
-          <View style={styles.expensesCategory}>
-            <View>
-              <Ionicons
-                name="apps-outline"
-                size={24}
-                style={styles.iconStyle}
-              />
-            </View>
+            <View style={styles.expensesCategory}>
+              <View>
+                <Ionicons
+                  name="apps-outline"
+                  size={24}
+                  style={styles.iconStyle}
+                />
+              </View>
 
-            <View style={styles.foodStyleContainer}>
-              <Text style={styles.textCategoryandExepenses}>Others</Text>
+              <View style={styles.foodStyleContainer}>
+                <Text style={styles.textCategoryandExepenses}>Others</Text>
+              </View>
+              <View style={styles.expensesContainer}>
+                <Text style={styles.expensesText}>P {othersExpenses ? Number(othersExpenses).toLocaleString() : "0"}</Text>
+              </View>
             </View>
-            <View style={styles.expensesContainer}>
-              <Text style={styles.expensesText}>P {othersExpenses}</Text>
-            </View>
-          </View>
           </TouchableOpacity>
 
         </View>
